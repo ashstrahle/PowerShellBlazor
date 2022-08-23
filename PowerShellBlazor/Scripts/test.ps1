ac97876abc48279232ae4d407739525b9da52b41
Ashley Strahle <a.strahle@me.com>
2022-08-23 14:56:09 +1000

    Init

---
+++ PowerShellBlazor/Scripts/test.ps1
@@ -0,0 +1,7 @@
+﻿for ($i=1; $i -le 5; $i++) {
+  Write-Progress "Loop $i - progress output (Write-Progress)"
+  Write-Output "Normal output text (Write-Output)"
+  Write-Warning "Here's some warning text (Write-Warning)"
+  Write-Error "Oh no, here's some error text (Write-Error)"
+  Start-Sleep -s 1
+}
---
+++ PowerShellBlazor/Services/IPowerShellService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PowerShellBlazor.Services
+{
+    public interface IPowerShellService
+    {
+        public List<string> Output { get; set; }
+        public Task RunScript(PowerShell shell, bool varwidth);
+
+        event EventHandler<List<string>> OutputChanged;
+    }
+}
